4187d5b66aa0652afdb0af3f1ec5fbc39c423fcb
shazoo2k <166059608+shazoo2k@users.noreply.github.com>
2024-04-05 10:09:48 +0000

    add the changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Shahzaib Niaz",
+  title: "Web Developer",
+  email: "shahzaib.niaz@studentambassadors.com",
+  gitHub: "shazoo2k",
+  instagram: "shazoo2k",
+  linkedIn: "Shahzaib Niaz",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "sbz02",
+  youTube: "Shahzaib Niaz",
 };
 
 const primaryColor = "#4E567E";
